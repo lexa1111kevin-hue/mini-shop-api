8a1e06328303ea1123621322f4d8c1e2a94687ec
lexa1111kevin-hue <lexa1111kevin@gmail.com>
2025-10-08 17:46:56 +0500

    CRUD для Products

---
+++ src/products/product.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
